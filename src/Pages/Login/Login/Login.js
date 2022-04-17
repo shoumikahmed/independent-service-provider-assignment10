2abb053511f15c1d22dc55fcaae884131e197acb
shoumikahmed <shoumikahmed103@gmail.com>
2022-04-17 16:10:18 +0600

    some error handleing is completed

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Button, Form, ToastContainer } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
 import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
 import auth from '../../../firebase.init';
@@ -7,29 +7,59 @@ import auth from '../../../firebase.init';
 
 const Login = () => {
 
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+    const [userInfo, setUserInfo] = useState({
+        email: "",
+        password: "",
+    })
+    const [errors, setErrors] = useState({
+        email: "",
+        password: "",
+    })
 
     const [
-        signInWithEmailAndPassword,
+        signInWithEmail,
         user,
         loading,
-        error,
+        hookError,
     ] = useSignInWithEmailAndPassword(auth);
 
     const handleEmailChange = event => {
-        setEmail(event.target.value)
+        const emailRegex = /\S+@\S+\.\S+/;
+        const validEmail = emailRegex.test(event.target.value)
+
+        if (validEmail) {
+            setUserInfo({ ...userInfo, email: event.target.value })
+            setErrors({ ...errors, email: "" })
+        }
+        else {
+            setErrors({ ...errors, email: "Invalid email" })
+            setUserInfo({ ...userInfo, email: "" })
+        }
     }
 
     const handlePasswordChange = event => {
-        setPassword(event.target.value)
+        const passwordRegex = /.{6,}/;
+        const validPassword = passwordRegex.test(event.target.value);
+
+        if (validPassword) {
+            setUserInfo({ ...userInfo, password: event.target.value });
+            setErrors({ ...errors, password: "" });
+        }
+        else {
+            setErrors({ ...errors, password: "Need 6 characters!" });
+            setUserInfo({ ...userInfo, password: "" })
+        }
     }
 
     const handleSubmit = event => {
         event.preventDefault()
 
-        console.log(email, password)
+        signInWithEmail(userInfo.email, userInfo.password)
     }
+
+    useEffect(() => {
+        console.log(hookError)
+    }, [hookError])
 
     return (
         <div className='container w-50 mx-auto'>
@@ -38,17 +68,20 @@ const Login = () => {
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Control onChange={handleEmailChange} type="email" placeholder="Enter email" required />
                 </Form.Group>
-
+                {errors?.email && <p className='text-danger'>{errors.email}</p>}
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Control onChange={handlePasswordChange} type="password" placeholder="Password" required />
                 </Form.Group>
+                {errors?.password && <p className='text-danger'>{errors.password}</p>}
                 <Button variant="success w-50 mx-auto d-block mb-2 rounded-pill" type="submit">
                     Login
                 </Button>
+                {/* {error && <p className='text-danger'>{error}</p>}
+                {hookError && <p className='text-danger'>{hookError?.message}</p>} */}
             </Form>
             <p className='mt-2'>New to Genius Car? <Link to='/register' className='text-success pe-auto text-decoration-none' >Please Register</Link></p>
             <p className='mt-2'>Forget Password? <button className='btn btn-link text-success pe-auto text-decoration-none' >Reset Password</button></p>
-            <ToastContainer />
+
         </div>
     );
 };
