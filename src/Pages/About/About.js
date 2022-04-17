78d9c2f1dafc16b33699e7bc6f62946efeec2fbb
shoumikahmed <shoumikahmed103@gmail.com>
2022-04-17 14:59:09 +0600

    about page completed

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -9,7 +9,9 @@ const About = () => {
                 <img className='w-100 container float-right' src={shoumik} alt="..." class="img-thumbnail" />
                 <div className='mt-3 text-center'>
                     <h3 className='text-success'>My Goal</h3>
-                    <p className='text-secondary '>My Goal is to be a Full Stack Web Developer</p>
+                    <p className='text-secondary '>Hi there, I'm Shoumik Ahmad. From the beginning, I set my goal as a full stack web developer. Basically, full stack is a combination of front-end and back-end development. It's very hard to prove oneself as a full stack cause It's time consuming and need continuous work for a long time.</p>
+                    <br />
+                    <p className='text-secondary '>My roadmap shows me a simple path. Front-end is the entrance. After absorbing this skill I want to set up myself for back end</p>
                 </div>
             </div>
 
