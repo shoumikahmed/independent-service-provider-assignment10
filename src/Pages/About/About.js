36ec07bf40db9526666462aec9ed8e0ffa3d3336
shoumikahmed <shoumikahmed103@gmail.com>
2022-04-18 00:01:59 +0600

    checkout page completed

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -11,7 +11,7 @@ const About = () => {
                     <h3 className='text-success'>My Goal</h3>
                     <p className='text-secondary '>Hi there, I'm Shoumik Ahmad. From the beginning, I set my goal as a full stack web developer. Basically, full stack is a combination of front-end and back-end development. It's very hard to prove oneself as a full stack cause It's time consuming and need continuous work for a long time.</p>
                     <br />
-                    <p className='text-secondary '>My roadmap shows me a simple path. Front-end is the entrance. After absorbing this skill I want to set up myself for back end</p>
+                    <p className='text-secondary '>My roadmap shows me a simple path. Front-end is the entrance. After absorbing this skill I want to set up myself for back end.</p>
                 </div>
             </div>
 
--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -1,10 +1,50 @@
 import React from 'react';
+import './Checkout.css'
 
 const Checkout = () => {
     return (
-        <div>
-            <h2>Please Checkout your booking</h2>
-        </div>
+        <section className='Check-Out'>
+            <h2 className='title'>Check Out</h2>
+            <div className='container'>
+                <div className="checkout-form row">
+                    <div className="form-field col-lg-6">
+                        <input type="text" className='input-text' name="" id="name" />
+                        <label htmlFor="name" className='label'>Name</label>
+                    </div>
+                    <div className="form-field col-lg-6">
+                        <input type="email" className='input-text' name="" id="email" />
+                        <label htmlFor="email" className='label'>Email</label>
+                    </div>
+                    <div className="form-field col-lg-6">
+                        <input type="text" className='input-text' name="" id="address1" />
+                        <label htmlFor="address1" className='label'>Address</label>
+                    </div>
+                    <div className="form-field col-lg-6">
+                        <input type="text" className='input-text' name="" id="address2" />
+                        <label htmlFor="address2" className='label'>Address (optional)</label>
+                    </div>
+                    <div className="form-field col-lg-6">
+                        <input type="number" className='input-text' name="" id="code" />
+                        <label htmlFor="code" className='label'>Post Code</label>
+                    </div>
+                    <div className="form-field col-lg-6">
+                        <input type="text" className='input-text' name="" id="country" />
+                        <label htmlFor="country" className='label'>Country</label>
+                    </div>
+                    <div className="form-field col-lg-6">
+                        <input type="text" className='input-text' name="" id="city" />
+                        <label htmlFor="city" className='label'>City</label>
+                    </div>
+                    <div className="form-field col-lg-6">
+                        <input type="number" className='input-text' name="" id="phone" />
+                        <label htmlFor="phone" className='label'>Phone Number</label>
+                    </div>
+                    <div className="form-field col-lg-12">
+                        <input type="submit" className='submit-btn' value="submit" />
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
