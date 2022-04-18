1d94a988ec63e8991bcace759c5cf55b7bc5c5ae
shoumikahmed <shoumikahmed103@gmail.com>
2022-04-18 14:20:30 +0600

    all style complete

--- src/Pages/Home/Example/Example.js
+++ src/Pages/Home/Example/Example.js
@@ -8,22 +8,22 @@ const Example = () => {
                 <div className="col-md-3 text-white p-4 details">
                     <h3 className='text-warning'>Phone</h3>
                     <p>If you want to hire me for photography you can contact with me</p>
-                    <p className='text-warning'>Phone: 01778-774082</p>
+                    <p><strong>Phone: 01778-774082</strong></p>
                 </div>
                 <div className="col-md-3 text-white p-4 details">
                     <h3 className='text-warning'>Meeting</h3>
                     <p>If you want to meet with me for photography we can bother</p>
-                    <p className='text-warning'>Address: R.N.Road, Jashore</p>
+                    <p><strong>Address: R.N.Road, Jashore</strong></p>
                 </div>
                 <div className="col-md-3 text-white p-4 details">
                     <h3 className='text-warning'>Checkout</h3>
                     <p>If you want hire me for photography you can go to checkout page</p>
-                    <p className='text-warning'>Click the card button</p>
+                    <p><strong>Click the card button</strong></p>
                 </div>
                 <div className="col-md-3 text-white p-4 details">
                     <h3 className='text-warning'>Sign In</h3>
                     <p>You can sign in in my website to explore me as a professional photographer</p>
-                    <p className='text-warning'>Go To: Sign In</p>
+                    <p><strong>Go To: Sign In</strong></p>
                 </div>
             </div>
         </section>
